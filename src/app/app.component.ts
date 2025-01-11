c9698bd8661490686c607993c9468cc48ad22a55
gabsK1ing <tyrodunuts@gmail.com>
2025-01-11 08:00:56 -0300

    add receberFormulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LandingPageComponent],
+  imports: [LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
 
-  alerta(): void {
-    alert("Ola Mundo, Você Clicou");
+  public formGroup: FormGroup;
+
+  constructor() {
+    this.formGroup = new FormGroup({
+      email: new FormControl("", [Validators.email, Validators.required]),
+      senha: new FormControl("", [Validators.required, Validators.maxLength(25), Validators.minLength(8)])
+    });
+  } 
+
+  public limparFormulario(): void {
+    this.formGroup.reset();
   }
 
-  semSaibaMais(): void {
-    alert("Ainda não temos o Saiba Mais");
+  public receberFormulario(): void {
+    const emailControl = this.formGroup.get('email');
+    const senhaControl = this.formGroup.get('senha');
+    if (emailControl && senhaControl) {
+      const nome = emailControl.value;
+      const senha = senhaControl.value;
+      console.log('Email recebido:', nome);
+      console.log("Senha recebida", senha);
+      this.limparFormulario();
+    } else {
+      console.error('O controle "email" ou a "senha" não foi encontrado ou está nulo.');
+    }
   }
+  
 }
+
+
+
+
