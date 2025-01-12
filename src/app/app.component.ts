e0df8a497b68fb6ace83dadcfab9581b6758bb7e
gabsK1ing <tyrodunuts@gmail.com>
2025-01-12 04:31:28 -0300

    remove module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RouterModule } from '@angular/router';
-
-
 
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPageComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
