4bc19c63139924acc09b7b822410e88faa1b2274
gabsK1ing <tyrodunuts@gmail.com>
2025-01-17 21:33:56 -0300

    add logic tokentrue

--- src/app/components/tokentrue/tokentrue.component.ts
+++ src/app/components/tokentrue/tokentrue.component.ts
@@ -10,35 +10,44 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tokentrue',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './tokentrue.component.html',
-  styleUrl: './tokentrue.component.css'
+  styleUrl: './tokentrue.component.css',
 })
 export class TokentrueComponent {
-
   public form: FormGroup;
 
   constructor(private http: HttpClient, private route: Router) {
     this.form = new FormGroup({
-      token: new FormControl('', [ Validators.required, Validators.minLength(36), Validators.maxLength(37)])
+      token: new FormControl('', [
+        Validators.required,
+        Validators.minLength(36),
+        Validators.maxLength(37),
+      ]),
     });
   }
 
   testarToken() {
-    if(this.form.valid) {
+    if (this.form.valid) {
+      const tokenData = this.form.get('token')?.value;
 
-      const tokenData = {
-         token: this.form.get('token')?.value
-      }
+      console.log(tokenData);
 
       const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-
-      this.http.post("http://localhost:8080/postToken", tokenData, {headers, responseType: 'text' }).subscribe(
-        
-          
-        
-      )
+      this.http 
+        .post(
+          'http://localhost:8080/postToken',
+          JSON.stringify(tokenData),
+          {
+            headers,
+            responseType: 'text',
+          }
+        )
+        .subscribe({
+          next: (response: string) => console.log(response),
+          error: (response: string) => console.log(response),
+        });
     }
   }
 }
